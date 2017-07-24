c03459c2934526ef3799232d181cefe6847a28a5
Martin Adams <heymartinadams@users.noreply.github.com>
2017-07-24 16:36:02 -0700

    Checks for and rejects if expired token.

--- server/passwordReset.js
+++ server/passwordReset.js
@@ -8,6 +8,23 @@ module.exports = function(event) {
   const api = graphcool.api('simple/v1')
   const saltRounds = 10
 
+  function getTokenExpiration(id) {
+    return api.request(`
+    query {
+      User(id: "${id}"){
+        id
+        resetExpires
+      }
+    }`)
+      .then(userQueryResult => {
+        if (userQueryResult.error) {
+          return Promise.reject(userQueryResult.error)
+        } else {
+          return userQueryResult.User
+        }
+      })
+  }
+
   function updateGraphcoolUser(id, newPasswordHash) {
     return api.request(`
       mutation {
@@ -21,8 +38,17 @@ module.exports = function(event) {
       .then(userMutationResult => (userMutationResult.updateUser.id))
   }
 
-  return bcrypt.hash(newPassword, saltRounds)
-    .then(hash => updateGraphcoolUser(userId, hash))
-    .then(id => ({ data: { id } }))
-    .catch(error => ({ error: error.toString() }))
+  return getTokenExpiration(userId)
+    .then(graphcoolUser => {
+      if (graphcoolUser === null) {
+        return Promise.reject("Invalid credentials")
+      } else if(new Date() > new Date(graphcoolUser.resetExpires)) {
+        return Promise.reject("Token expired")
+      } else {
+        return bcrypt.hash(newPassword, saltRounds)
+          .then(hash => updateGraphcoolUser(userId, hash))
+          .then(id => ({ data: { id } }))
+          .catch(error => ({ error: error.toString() }))
+      }
+    })
 }
